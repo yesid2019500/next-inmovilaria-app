502a16c1f04fb72728d58f543e2176d08e7df7c8
Yessi Angulo <alexanderangulo917@gmail.com>
2022-11-04 06:04:23 -0500

    up two

--- components/Container.jsx
+++ components/Container.jsx
@@ -6,15 +6,13 @@ import Navigation from './navigation'
 const Container = (props) => {
   return (
     <>
-
         <Head>
-            <title>Next curso</title>   
+            <title>Inmoviliaria App</title>   
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css" />
         </Head>
-
         <Navigation/>
        
-        <div  >
+        <div>
             { props.children }
         </div>
     </>
